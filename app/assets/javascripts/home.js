1061c8f3fbb056571bca1ad796ff248d23867169
Heneman <kyle.heneman@gmail.com>
2014-07-31 11:46:21 -0500

    Changed Like Box to iFrame and added 100% width to iFrame inline style

--- app/assets/javascripts/home.js
+++
@@ -1,9 +0,0 @@
-$('document').ready(function(){
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=147219862027659&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-});
