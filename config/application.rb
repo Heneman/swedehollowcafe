b47a3856b30b2054cbd1b32d6152aca0b98e2981
Heneman <kyle.heneman@gmail.com>
2014-08-02 13:55:28 -0500

    Broke menu down into partials

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,11 @@ module SwedeHollowCafe
     config.assets.precompile.unshift -> (path) {
       (extension = File.extname(path)).present? and extension.in?(precompile_whitelist)
     }
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
     
   end
 end
