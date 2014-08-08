88bab30f48e2da49f87bdc3fff626acca088b755
Heneman <kyle.heneman@gmail.com>
2014-08-08 10:43:59 -0500

    Refactored menu.js, made Bakery menu default view

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,33 +1,31 @@
 $(document).ready(function(){
 
-  // TODO: Find better way to do this
-
   $("#bakery-link").click(function(){
     $("#bakery").removeClass("hidden");
-    $("#breakfast").addClass("hidden");
-    $("#lunch").addClass("hidden");
-    $("#beverages").addClass("hidden");
+    $("#breakfast, #lunch, #beverages").each(function(){
+      $(this).addClass("hidden");
+    });
   });
 
   $("#breakfast-link").click(function(){
-    $("#bakery").addClass("hidden");
     $("#breakfast").removeClass("hidden");
-    $("#lunch").addClass("hidden");
-    $("#beverages").addClass("hidden");
+    $("#bakery, #lunch, #beverages").each(function(){
+      $(this).addClass("hidden");
+    });
   });
 
   $("#lunch-link").click(function(){
-    $("#bakery").addClass("hidden");
-    $("#breakfast").addClass("hidden");
     $("#lunch").removeClass("hidden");
-    $("#beverages").addClass("hidden");
+    $("#bakery, #breakfast, #beverages").each(function(){
+      $(this).addClass("hidden");
+    });
   });
 
   $("#beverages-link").click(function(){
-    $("#bakery").addClass("hidden");
-    $("#breakfast").addClass("hidden");
-    $("#lunch").addClass("hidden");
     $("#beverages").removeClass("hidden");
+    $("#bakery, #breakfast, #lunch").each(function(){
+      $(this).addClass("hidden");
+    });
   });
 
 });
