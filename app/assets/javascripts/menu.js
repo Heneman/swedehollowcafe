db0e3b8352e93cd7b5544bf9c4277627908b9c47
Heneman <kyle.heneman@gmail.com>
2014-08-14 12:30:15 -0500

    I mean NOW fade in/out works properly

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-
   $(".menu-link").click(function(){
     $(".menu-link").not( $(this) ).each(function(){
       $( "#" + $(this).text().toLowerCase() + "-menu" ).fadeOut().addClass("hidden");
     });
     
-    $( "#" + $(this).text().toLowerCase() + "-menu" ).removeClass("hidden").fadeIn();
+    $( "#" + $(this).text().toLowerCase() + "-menu" ).fadeIn().removeClass("hidden");
   });
 
 });
