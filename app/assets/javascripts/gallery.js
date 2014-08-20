733f866f20c3b67b74ef14a2522db7452c5b9e2c
Heneman <kyle.heneman@gmail.com>
2014-08-19 21:11:38 -0500

    Added new images, changed front page image, disabled Instafeed

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -5,35 +5,41 @@
 //= require instafeed.min.js
 
 
-// Use (window).on('load') instead of 'ready'
-// so images have time to load before
-// Masonry touches them
+$(document).ready(function(){
 
-$(window).on("load", function(){
+  // var before = [];
+  // var after = [];
+  // var diff = [];
 
-  var feed = new Instafeed({
-    get: "tagged",
-    tagName: "swedehollow",
-    clientId: "f929602ce1cf479c9a287c72e9677304",
-    max: 20,
-    links: false,
-    resolution: "standard_resolution",
-    target: "masonry",
-    sortBy: "most-recent",
-    template: '<div class="image-box col-sm-4 col-md-4 col-lg-4">' +
-              '<a class="fancybox" rel="gallery">' +
-              '<img class="img-responsive img-thumbnail" src="{{image}}">' +
-              '</a>' +
-              '</div>',
-    after: function(){
-      $(".masonry").masonry({
-        itemSelector: ".image-box",
-        sortBy: "random"
-      });
-    }
-  });
+  // var feed = new Instafeed({
+  //   get: "tagged",
+  //   tagName: "swedehollow",
+  //   clientId: "f929602ce1cf479c9a287c72e9677304",
+  //   max: 20,
+  //   links: false,
+  //   resolution: "standard_resolution",
+  //   target: "masonry",
+  //   sortBy: "most-recent",
+  //   template: '<div class="image-box col-sm-4 col-md-4 col-lg-4">' +
+  //             '<a class="fancybox" href="{{image}}" rel="gallery">' +
+  //             '<img class="img-responsive img-thumbnail" src="{{image}}">' +
+  //             '</a>' +
+  //             '</div>',
+  //   after: function(){
+  //     if (!this.hasNext()) {
+  //       $("#load-more").setAttribute('disabled', 'disabled');
+  //     }
 
-  feed.run();
+  //     after = $(".image-box");
+
+  //     diff = $(after).not(before).get();
+
+  //     $(".masonry").masonry("appended", diff);
+
+  //     $(".masonry").masonry();
+
+  //   }
+  // });
 
   $(".fancybox").fancybox({
     helpers: {
@@ -44,4 +50,26 @@ $(window).on("load", function(){
       }
     }
   });
-})
+
+  // $("#load-more").on("click", function(){
+  //   before = $(".image-box");
+
+  //   feed.next();
+
+  //   $(".masonry").masonry();
+  // });
+
+  // feed.run();
+
+});
+
+// Use (window).on('load') instead of 'ready'
+// so images have time to load before
+// Masonry touches them
+
+$(window).on("load", function(){
+  $(".masonry").masonry({
+    itemSelector: ".image-box"
+  });
+});
+
