f23cfdb5eb076e4b835a97d09b6d774f502ed93e
Heneman <kyle.heneman@gmail.com>
2014-08-26 15:23:27 -0500

    Added images to menu-nav

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $(".menu-link").click(function(){
+
     $(".menu-link").not( $(this) ).each(function(){
       $( "#" + $(this).text().toLowerCase() + "-menu" ).fadeOut().addClass("hidden");
     });
-    
     $( "#" + $(this).text().toLowerCase() + "-menu" ).fadeIn().removeClass("hidden");
+    
   });
 
 });
