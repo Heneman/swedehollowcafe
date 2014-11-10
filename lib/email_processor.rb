6273dfb0ca6e84f5d436d55726e530ab869956fa
Heneman <kyle.heneman@gmail.com>
2014-11-10 17:27:34 -0600

    Testing email

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -4,14 +4,12 @@ class EmailProcessor
   end
 
   def process
-    puts "Inpection #1 " + @email.inspect
     Mail.deliver do
-      puts "Inspection #2 " + @email.inspect
       to 'kyle.heneman@gmail.com'
-      from @email.from
-      subject @email.subject
-      body @email.body
-      @email.attachments.each { |attachment|
+      from self.email.from
+      subject self.email.subject
+      body self.email.body
+      self.email.attachments.each { |attachment|
         add_file :filename => File.basename(attachment), :content => attachment
       }
     end
