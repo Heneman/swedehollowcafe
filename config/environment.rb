9313d03ea882c05312da10f043589e670157ce5f
Heneman <kyle.heneman@gmail.com>
2014-11-10 15:55:11 -0600

    Using Mail gem instead of ActionMailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Use ActionMailer for SMTP
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  #port 465 for SSL
-  :port           => '465',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :ssl            => true
-}
-
 # Use Mail to forward incoming email
 require 'mail'
 
